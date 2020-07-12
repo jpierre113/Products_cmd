f020a1270f0447cdbf0cd422e003d353f725a4de
Janna Pierre <gojanna@aol.com>
2020-07-12 17:07:16 -0400

    Removed Option Parser and created tshirt filter

--- cli.rb
+++ cli.rb
@@ -1,16 +1,9 @@
 #!/usr/bin/env ruby
-require 'optparse'
 require 'json'
 
 file = File.read("products.json")
 #data stores the products objects
 data = JSON.load file
 
-
-products = {}
-OptionParser.new do |prod|
-    prod.on("-p", "--product PRODUCT") do |type|
-        products[:product] = type
-    end
-end.parse!
-puts products[:product]
+shirt_only = data.select{|product| product['product_type'] == "tshirt"}
+puts shirt_only
