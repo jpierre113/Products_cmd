3762ea7e1449153aeeef0e139afb07bda180a15d
Janna Pierre <gojanna@aol.com>
2020-07-13 16:02:23 -0400

    Added more filters and conditional to display output

--- cli.rb
+++ cli.rb
@@ -8,13 +8,19 @@ data = JSON.load file
 choice = gets.chomp
 
 firstFilter = data.select{|product| product['product_type'] == choice}
+
 #accessing options hash
 getGender = firstFilter.map{|gender| gender['options']['gender']}.compact.uniq
 getColor = firstFilter.map{|color| color['options']['color']}.compact.uniq
 getSize = firstFilter.map{|size| size['options']['size']}.compact.uniq
+getType = firstFilter.map{|type| type['options']['type']}.compact.uniq
+getStyle = firstFilter.map{|style| style['options']['style']}.compact.uniq
 
-# firstFilter.each do |type|
-#     firstFilter = type['options']
-# end
-
-print getGender + getColor + getSize
+#based on filter display output
+if getGender == true || getColor == true || getSize == true
+    print "gender: #{getGender} color: #{getColor}  size: #{getSize}"
+elsif getType == true
+  print "type: #{getType}"
+else
+  print "size: #{getSize} style: #{getStyle}"
+end
