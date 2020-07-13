d441aaaa6a49248f7077e059e7de2b337638866d
Janna Pierre <gojanna@aol.com>
2020-07-13 11:42:37 -0400

    Able to access gender in options hash

--- cli.rb
+++ cli.rb
@@ -8,4 +8,7 @@ data = JSON.load file
 choice = gets.chomp
 
 firstFilter = data.select{|product| product['product_type'] == choice}
+firstFilter.each do |type|
+    firstFilter = type['options'] && type['options']['gender']
+end
 puts firstFilter
